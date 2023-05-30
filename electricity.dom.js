4bb9a6005d9e0060d12cad680a79e77e15bed7bd
Londeka Zikalala <londekazikalala@gmail.com>
2023-05-30 13:14:22 +0200

    DOM elements added

--- electricity.dom.js
+++ electricity.dom.js
@@ -1,4 +1,13 @@
 // DOM element(s) references
+topupElement = document.querySelector('.topup');
+BuyBtnElement = document.querySelector('.topupNow');
+topupAdvanceElement = document.querySelector('.topupAdvance');
+unitsAvailableElement= document.querySelector('.unitsAvailable')
+unitsBoughtElement = document.querySelector('.totalUnits');
+amountSpentElement= document.querySelector('.totalAmount');
+advanceTakenElement = document.querySelector('.advanceTaken');
+usageElement = document.querySelector('.usage');
+useNowBtnElement = document.querySelector('.useNow');
 
 
 
@@ -6,4 +15,7 @@
 const electricity =  Electricity();
 
 
+
 // DOM events here 
+BuyBtnElement.addEventListener('click', buyNow)
+useNowBtnElement.addEventListener('click', useNow)
--- electricity.js
+++ electricity.js
@@ -1,7 +1,8 @@
 function Electricity() {
 var totalAmount = 0;
-var totalUnits = 0;
 var unitsAvailable = 0;
+var advance = false;
+var advanceAmount = 0;
     // do we want to go with this or array? 
     let appliances = {
         'Stove': 10, 
@@ -10,15 +11,22 @@ var unitsAvailable = 0;
         'Fridge': 13
     };
 /*The topUpElectricity function checks for the type of amount loaded using an if statement and the === operator
-Then adds the value to the total amount*/
+Then adds the value to the total amount if the advance is already taken, no units can be loaded*/
     function topUpElectricity(amount) {
     if(amount === 'advance'){
-        totalAmount += 30
-}
-    else if (typeof amount === 'number'){
+    if(advance){
+        return;
+    }else{
+        totalAmount+=30;
+        advance = true;
+        advanceAmount +=30;
+    }
+} else if(typeof amount === 'number'){
     totalAmount += amount
 }
-    }
+    
+}
+
 /*This function calculates the amount of units, uses if staments and === to check for the value and assign the total units for that value*/
     function getUnitsAvailable() {
         if(totalAmount === 30){
@@ -44,29 +52,25 @@ Then adds the value to the total amount*/
     * other wise return false and do nothing.
     */
     function useAppliance(appliance) {
-        for(var units in appliances){
-            var applianceUsage = appliances[units]
-            if(units === appliance){
-                if(unitsAvailable >= applianceUsage){
-                    unitsAvailable -= applianceUsage;
-                    return true
-                } else{
-                    return false
-                }
-            }
+        var applianceUsage = appliances[appliance];
+        if (applianceUsage !== undefined && unitsAvailable >= applianceUsage) {
+          unitsAvailable -= applianceUsage;
+          return true;
+        } else {
+          return false;
         }
-        return false
-    }
+      }
 /*This function returns the total advance taken */
     function advanceTaken() {
+        return advanceAmount.toFixed(2)
     }
 
     function totalAmountSpent() {
+        return totalAmount.toFixed(2)
     }
 
-    function totalUnitsBought(){
-        totalUnits += unitsAvailable
-        return totalUnits
+    function totalUnitsBought(){ 
+        return  (totalAmount/1.4285714286).toFixed(2)
     }
 
     return {
